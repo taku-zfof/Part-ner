e99997a151834916694a8b3f988d602b810527f9
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-05-04 17:33:57 +0000

    MDbootstrap動作確認,deviseUser作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,15 +2,17 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import * as mdb from 'mdb-ui-kit';
+import './mdb'
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import * as mdb from 'mdb-ui-kit';
-import './mdb'
+
 import "jquery";
 import "popper.js";
+import "bootstrap"; //bootstrap本体
 import "../stylesheets/application" 
 import '@fortawesome/fontawesome-free/js/all'
 
