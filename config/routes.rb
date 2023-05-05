01cc1156303622789f9f31346808e4c28fddf05e
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-05-05 10:39:01 +0000

    [UPDATE]deviseのビューファイルをslim化

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   devise_for :users,skip: [:passwords]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
