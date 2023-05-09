afed4b11e1434cb0f167e947b2444541da5d7387
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-05-09 23:40:58 +0000

    [ADD]DM一覧に未読件数表示、showで既読にする機能

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,15 +1,24 @@
 class ChatroomsController < ApplicationController
   def index
+    #自分の作成した仕事のidをすべて取得
     myjob_ids = Job.where(user_id: current_user.id).pluck(:id)
     
     #自分がオファーしたものor自分の仕事に紐づいてるチャットルームをすべて取得
     @chatrooms = Chatroom.where(job_id: myjob_ids).or(Chatroom.where(user_id: current_user.id))
+    
+    # @chatrooms each do |chatroom|
+    #   @partner_unread_messages = chatroom.messages.where.not(user_id: current_user.id).where(read_status: false)
+    # end
   end
 
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
     @messages = Message.where(chatroom_id: @chatroom.id)
+    
+    #チャット相手のメッセージをすべて既読にする
+    partner_messages = @messages.where.not(user_id: current_user.id)
+    partner_messages.update(read_status: true)
   end
 
   def create
