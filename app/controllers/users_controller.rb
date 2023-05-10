16325d8249222d22f5362a0f1058d9756d964986
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-05-10 05:46:23 +0000

    [ADD]DM一覧のレイアウト作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+   before_action :ensure_guest_user, only: [:edit]
 
   def show
     @user=User.find(params[:id])
@@ -24,7 +25,7 @@ class UsersController < ApplicationController
   def ensure_guest_user
     @user = User.find(params[:id])
     if @user.name == "guestuser"
-      redirect_to current_user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
     end
   end  
 end
