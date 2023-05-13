294ff546510a6790a340dde0e9dca1e0dc45df32
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-05-13 07:49:57 +0000

    [ADD]テストデプロイ用設定

--- db/migrate/20230506095515_change_column_to_null.rb
+++ db/migrate/20230506095515_change_column_to_null.rb
@@ -3,7 +3,7 @@ class ChangeColumnToNull < ActiveRecord::Migration[6.1]
     def up
     # Not Null制約を外す場合　not nullを外したいカラム横にtrueを記載
     change_column_null :users, :introduction, true
-  end
+    end
 
   def down
     change_column_null :users, :introduction, false
