313ded1e499133ae322ec1032edddb442fae680b
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-05-25 00:24:43 +0000

    [FIX]本番環境用メール設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,8 +117,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
