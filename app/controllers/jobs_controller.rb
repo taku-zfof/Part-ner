4b8a04748a0ae87a732b6ff217fbe535be6c204f
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-06-08 23:59:59 +0900

    [fix]jobscontroller リファクタリング

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,8 @@
 
 class JobsController < ApplicationController
   before_action :ensure_user, only:[:edit, :update, :destroy]
-
+  before_action :set_job, only:[:show, :edit, :update, :destroy]
+  
   def new
     @job=Job.new
     @renderd = "renderd"
@@ -34,40 +35,30 @@ class JobsController < ApplicationController
     end
   end
 
-
-
   def show
-    @job=Job.find_by(rondom_id: params[:rondom_id])
   end
-
-
 
   def edit
-    @job=Job.find_by(rondom_id: params[:rondom_id])
   end
-
-
 
   def update
     job=Job.find_by(rondom_id: params[:rondom_id])
     if params[:post] # 投稿する場合
-        job.attributes = job_params # updateにcontextが適用できないためattributeで代入してからsave
-        job.released = true
-        if job.save(context: :release)
+        @job.attributes = job_params # updateにcontextが適用できないためattributeで代入してからsave
+        @job.released = true
+        if @job.save(context: :release)
           # 最寄り駅情報を取得して代入
-          job.addStation
+          @job.addStation
           redirect_to job_path(job),flash: {notice: "募集を公開しました"}
         else
-          @job=job
           flash.now[:error] = "公開できませんでした"
           render :edit
         end
     elsif params[:draft] # 下書きにする場合
-        job.released = false
-        if job.update(job_params)
+        @job.released = false
+        if @job.update(job_params)
           redirect_to job_draft_index_path, flash: {notice: "募集を下書き保存しました"}
         else
-          @job=job
           flash.now[:error] = "下書き保存に失敗しました"
           render :edit
         end
@@ -76,15 +67,15 @@ class JobsController < ApplicationController
 
 
   def destroy
-    job=Job.find_by(rondom_id: params[:rondom_id])
-    if job.destroy
+    if @job.destroy
       redirect_to user_path(current_user), flash: {notice: "募集を削除しました"}
     else
       flash.now[:error] = "削除できませんでした"
       render :edit
     end
   end
-
+  
+  #下書き一覧
   def draft_index
     @jobs = current_user.jobs.where(released: false).all
     @jobs = Kaminari.paginate_array(@jobs).page(params[:page]).per(5) # ページネーション（無限スクロール用）
@@ -93,7 +84,7 @@ class JobsController < ApplicationController
 
   def search
     @jobs = Job.where(released: true).all
-    # 条件が存在する場合のみ絞り込む
+    # 各条件が存在する場合のみ絞り込む
     @jobs = @jobs.where(prefecture_code: params[:prefecture_code]) if params[:prefecture_code].present?
     @jobs = @jobs.where(job_type: params[:job_type]) if params[:job_type].present?
     @jobs = @jobs.where("tytle LIKE(?) or introduction LIKE(?)","%#{params[:key_word]}%","%#{params[:key_word]}%") if params[:key_word].present?
@@ -105,6 +96,11 @@ class JobsController < ApplicationController
 private
   def job_params
     params.require(:job).permit(:rondom_id, :tytle, :job_type, :image, :introduction, :hourly_wage, :postal_code, :prefecture_code, :other_address ,:near_station, :near_station_line)
+  end
+  
+  #jobをランダムIDで特定
+  def set_job
+    @job=Job.find_by(rondom_id: params[:rondom_id])
   end
   
   # 自分の投稿以外は編集不可
